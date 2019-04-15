63fe4a7cf803b3b9a16a489498d38573ae5e4ab3
404-html <45104241+404-html@users.noreply.github.com>
2019-04-15 23:00:09 +0200

    Internet cleaning
    Powered by [Internet Cleaner®️](https://github.com/404-html/internet-cleaner)

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -3514,7 +3514,7 @@ int skb_cow_data(struct sk_buff *skb, int tailbits, struct sk_buff **trailer)
 		    skb_has_frag_list(skb1)) {
 			struct sk_buff *skb2;
 
-			/* Fuck, we are miserable poor guys... */
+			/* Butterfly, we are miserable poor guys... */
 			if (ntail == 0)
 				skb2 = skb_copy(skb1, GFP_ATOMIC);
 			else
